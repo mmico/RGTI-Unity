07ba0e5d7fafd8301770f6d0b121ad3b1e6f8e46
mmico <mico.maren@gmail.com>
2019-01-04 12:51:25 +0100

    World in one object, raycast for key

---
+++ Underground/Assets/Scripts/Raycast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1.0f))
+        {
+            if (hit.collider.gameObject.tag == "Key") {
+                if (Input.GetKey(KeyCode.E))
+                {
+                    hit.collider.gameObject.GetComponent<Key>().Interact();
+                }
+            }
+        }
+    }
+}
