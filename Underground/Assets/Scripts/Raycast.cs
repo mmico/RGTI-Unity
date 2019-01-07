ed0989fc311b26261dbd64f532753e29f765597d
mmico <mico.maren@gmail.com>
2019-01-07 16:17:12 +0100

    Raycast fix, water gate animation

--- Underground/Assets/Scripts/Raycast.cs
+++ Underground/Assets/Scripts/Raycast.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class Raycast : MonoBehaviour
 {
     RaycastHit hit;
+    private new Transform camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = this.gameObject.transform.GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(GameState.canOpenDoor.Count);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2.0f))
+        if (Physics.Raycast(camera.transform.position, camera.transform.TransformDirection(Vector3.forward), out hit, 2.0f))
         {
             if (hit.collider.gameObject.tag == "Key") {
                 if (Input.GetKey(KeyCode.E))
