834dee3e7d9ead723dcf77c8b761f09dae7cdd48
mmico <mico.maren@gmail.com>
2019-01-09 15:09:37 +0100

    Pull problem

---
+++ Underground/Assets/Scripts/Rat_Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rat_Move : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+    }
+}
