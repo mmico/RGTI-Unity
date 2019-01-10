381132b57997910fd935ff3ee493443ca19acaab
YoloHanSolo <jnpelicon@gmail.com>
2019-01-10 17:02:09 +0100

    podgane + collision fix

--- Underground/Assets/Scripts/Rat_Move.cs
+++ Underground/Assets/Scripts/Rat_Move.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Rat_Move : MonoBehaviour
 {
-    public float moveSpeed;
 
-    void Start()
-    {
-        
-    }
+    public GameObject[] waypoints;
+    public int num = 0;
+
+    public float speed;
+    public float minDist;
 
     void Update()
     {
-        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        float dist = Vector3.Distance(gameObject.transform.position, waypoints[num].transform.position);
+
+        if (dist > minDist)
+        {
+            Move();
+        }
+        else
+        {
+            if (num + 1 == waypoints.Length)
+            {
+                num = 0;
+            }
+            else
+            {
+                num++;
+            }
+        }
+
     }
+
+    public void Move()
+    {
+        transform.LookAt(waypoints[num].transform.position);
+        transform.eulerAngles = new Vector3(0, transform.rotation.eulerAngles.y - 90, 0);
+        transform.position += gameObject.transform.right * speed * Time.deltaTime;
+    }
+
 }
+
+
